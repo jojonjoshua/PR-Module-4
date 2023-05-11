6f2488d2cb9d2e66847a544bdfb0d8e61c913410
jojonjoshua <133229159+jojonjoshua@users.noreply.github.com>
2023-05-11 23:56:28 +0700

    commit 4

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello temen onie");
+        System.out.println("Hello temen temen wongkai");
     }
 }
